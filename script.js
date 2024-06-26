797a341e95f46e330f2f43fb6dd8e02b587b17f4
ashish-peerbits <ashish.p@peerbits.com>
2024-06-26 18:23:43 +0530

    add show/hide column feature

--- script.js
+++ script.js
@@ -158,6 +158,54 @@ const dateEditor = function (cell, onRendered, success, cancel) {
     return input;
   };
 
+
+// header menu ---------------------
+const headerMenu = function(){
+    var menu = [];
+    var columns = this.getColumns();
+
+    for(let column of columns){
+
+        //create checkbox element using font awesome icons
+        let icon = document.createElement("i");
+        icon.classList.add("fas");
+        icon.classList.add(column.isVisible() ? "fa-check-square" : "fa-square");
+
+        //build label
+        let label = document.createElement("span");
+        let title = document.createElement("span");
+
+        title.textContent = " " + column.getDefinition().title;
+
+        label.appendChild(icon);
+        label.appendChild(title);
+
+        //create menu item
+        menu.push({
+            label:label,
+            action:function(e){
+                //prevent menu closing
+                e.stopPropagation();
+
+                //toggle current column visibility
+                column.toggle();
+
+                //change menu item icon
+                if(column.isVisible()){
+                    icon.classList.remove("fa-square");
+                    icon.classList.add("fa-check-square");
+                }else{
+                    icon.classList.remove("fa-check-square");
+                    icon.classList.add("fa-square");
+                }
+            }
+        });
+    }
+
+   return menu;
+};
+// ------------------------------------------------
+
 // Tabulator columns definition
 const tableColumns = [
   {
@@ -170,6 +218,9 @@ const tableColumns = [
     cellClick: function (e, cell) {
       cell.getRow().toggleSelect();
     },
+    
+    // header menu show/hide columns
+    headerMenu:headerMenu,
   },
   { title: "ID", field: "id", width: 80 },
   {
